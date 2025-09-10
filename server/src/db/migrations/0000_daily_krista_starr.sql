7c5e53daed7ebcc4cb04b93f5027187cbdc77833
darianmorat <yojhandariantoledomora@gmail.com>
2025-09-10 10:28:24 -0500

    fix(db): update migrations

---
+++ server/src/db/migrations/0000_daily_krista_starr.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(60) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "activities" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"date" varchar(255) NOT NULL,
+	"time" varchar(255) NOT NULL,
+	"image" varchar(255) NOT NULL,
+	"description" varchar(255) NOT NULL,
+	"spots" varchar(255) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "artworks" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"price" varchar(255) NOT NULL,
+	"size" varchar(255) NOT NULL,
+	"year" varchar(255) NOT NULL,
+	"image" varchar(255) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
