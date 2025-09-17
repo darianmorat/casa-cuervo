7b8b475982dbbb3204b8c69fc41727288eda8c44
darianmorat <yojhandariantoledomora@gmail.com>
2025-09-17 11:53:25 -0500

    fix(db): update migrations

--- server/src/db/migrations/0000_ordinary_vargas.sql
+++ server/src/db/migrations/0000_ordinary_vargas.sql
@@ -21,6 +21,8 @@ CREATE TABLE "activities" (
 CREATE TABLE "artworks" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"title" varchar(255) NOT NULL,
+	"category" varchar(255) NOT NULL,
+	"technique" varchar(255) NOT NULL,
 	"price" varchar(255) NOT NULL,
 	"size" varchar(255) NOT NULL,
 	"year" varchar(255) NOT NULL,
---
+++ server/src/db/migrations/0001_clammy_mesmero.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "artworks" ADD COLUMN "available" boolean DEFAULT true NOT NULL;
